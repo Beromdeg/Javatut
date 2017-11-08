d598ba0acee222cb1cce7b8521d213e8c5ee0663
Beromdeg <30961531+Beromdeg@users.noreply.github.com>
2017-11-08 15:42:09 +0100

    This is my second test commit

--- colorChoice/src/colorChoice/TestCC.java
+++ colorChoice/src/colorChoice/TestCC.java
@@ -2,7 +2,8 @@ package colorChoice;
 import javax.swing.JFrame;
 
 public class TestCC {
-
+//this is my second commit to test
+//if the fetch and push was ok 
 	public static void main(String[] args) {
 		ColorChoose go = new ColorChoose(); //ColorChoose object
 		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
